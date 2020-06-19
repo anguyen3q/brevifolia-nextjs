43f0c6229d75d1104bc1dae72dae6b7837e1b452
Anthony Nguyen <anguyen@3qdigital.com>
2020-06-19 14:14:09 -0700

    adding back optimizely

--- components/Meta.js
+++ components/Meta.js
@@ -4,6 +4,7 @@ export default function Meta(props) {
     return (
         <>
             <Head>
+            <script src="https://cdn.optimizely.com/js/18206491045.js"></script>
             <script type="text/javascript" src="https://try.abtasty.com/efa7865db2b993fd11757b2b3690d7ab.js"></script>
 
             
